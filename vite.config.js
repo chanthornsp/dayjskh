26e94bc3b86557a05430511d2bf5d19d950b83a3
chanthornsp <hoeurnchanthorn@gmail.com>
2026-02-10 10:33:32 +0700

    fix: update package.json and vite.config.js for improved module exports and build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 // vite.config.js
+import { resolve } from "path";
+
 export default {
   build: {
-    outDir: "dist/index.js",
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "dayjskh",
+      fileName: (format) => `dayjskh.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["dayjs"],
+      output: {
+        globals: {
+          dayjs: "dayjs",
+        },
+      },
+    },
   },
 };
